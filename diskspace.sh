e560837daf175edd9c003c2bdadde9e54b90493b
Konstantin Poslanichenko <swarthevdi@gmail.com>
2021-11-28 21:16:53 +0300

    Add adduser.sh, change diskspace.sh

--- diskspace.sh
+++ diskspace.sh
@@ -8,7 +8,7 @@ trap "rm -f $tempfile" EXIT
 cat << 'EOF' > $tempfile
     { sum += $4 }
 END { mb = sum / 1024
-      gb = mb /1024
+      gb = mb / 1024
       printf "%.0f MB (%.2fGB) of available disk space\n", mb, gb
     }
 EOF
